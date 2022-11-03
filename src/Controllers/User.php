dfe9d59305738f7e94cd403db94ec91d9e9d5536
Victor Efros <efrosvictor424@gmail.com>
2022-11-03 00:16:56 +0000

    I deleted the function that referred to the token

--- src/Controllers/User.php
+++ src/Controllers/User.php
@@ -4,14 +4,18 @@ namespace SRC\Controller;
 
 use SRC\Repository\UserRepository;
 use SRC\Configuration\Config;
+use SRC\Controller\TokenController;
 
 class User 
 {
     private $userRep;
+    
+    private $token;
 
     public function __construct()
     {
         $this->userRep = new UserRepository();
+        $this->token = new TokenController();
     }
 
     public function registerUser($data)
@@ -25,7 +29,7 @@ class User
         
         $data['role'] = (isset($data['role'])) ?  1 : 0;
 
-        $token = $this->generateToken($this->generateString());
+        $token =  $this->token->generateToken( $this->token->generateString());
         $user = $this->userRep->insertUser($data, $token); 
 
         if($user == false){
@@ -61,7 +65,7 @@ class User
             ];
         }
         
-        $token = $this->generateToken($this->generateString());
+        $token =  $this->token->generateToken( $this->token->generateString());
         $insertedToken = $this->userRep->insertTokenToUser($user['id'], $token);
         
         if($insertedToken == false){
@@ -72,6 +76,7 @@ class User
         }
 
         $user['token'] = $token;
+        unset($user['id']);
         return [
             'Status' => 'Success',
             'Message' => 'Usere Loged',
@@ -129,48 +134,4 @@ class User
             'Message' => "The data has not been updated",
         ];
     }
-
-    public function generateToken($word, $n = 7)
-    {
-   
-        $lowerLetters = 'abcdefghijklmnopqrstuvwxyz';
-        $arrLetters = str_split($lowerLetters);
-        $resArr = [];
-        for($i = 0; $i < strlen($word); $i++){
-            $val = array_search($word[$i],$arrLetters) + 1;
-            for($j = 0; $j < $n; $j++){
-                $val *= 3;
-                $val -= 5;
-            }
-            $resArr[] = $val;
-        }
-        return $resArr;
-    }
-    public function decrypt($word, $n)
-    {
-        $lowerLetters = 'abcdefghijklmnopqrstuvwxyz';
-        $arrLetters = str_split($lowerLetters);
-        $resArr = [];
-        for($i = 0; $i < count($word); $i++){
-            $num = $word[$i];
-            for($j = 0; $j < $n; $j++){
-                $num += 5;
-                $num /= 3;
-            }
-            $resArr[] =$arrLetters[$num -1];
-        }
-        return implode('',$resArr);
-    }
-
-    public function generateString($n = 10){
-        $characters = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM';
-        $randomString = '';
- 
-        for ($i = 0; $i < $n; $i++) {
-            $index = rand(0, strlen($characters) - 1);
-            $randomString .= $characters[$index];
-        }
-        return substr_replace($randomString, Config::$code, rand(1, $n), 0);
-    }
-
 }
